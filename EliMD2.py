bff4042dd64ef979dc5a9b9996c5b58f02eb831a
elimd15 <9671096804eli@gmail.com>
2021-06-11 21:26:46 -0500

    Create EliMD2.py
    Subi mi archivo

---
+++ EliMD2.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import Optional
+
+app = FastAPI()
+
+
+class Item(BaseModel):
+    name: str
+    price: float
+    is_offer: Optional[bool] = None
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
